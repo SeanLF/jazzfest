6a89ef7150ffb94da677f9528f4b79f929b6fbaf
Sean Floyd <seanlouisfloyd@gmail.com>
2017-02-18 18:55:42 -0500

    improve flow
    - when the user clicks 'apply' on an volunteer opportunity, the application should automatically select it and rank it as top choice as soon as they're authenticated if they haven't applied yet

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,14 @@
 class DashboardController < ApplicationController
+  before_action :store_opportunity_id_if_not_authorized_yet, only: [:show]
   before_action :authenticate_user!
   after_action :skip_authorization
 
   def show
     @profile = Profile.find_by(auth0_id: session[:userinfo]['uid'])
-    @opportunity_application = OpportunityApplication.find_or_create_by(profile: @profile)
-    @volunteer_opportunities = VolunteerOpportunity.all.order(:title)
+    @opportunity_application = OpportunityApplication.find_or_initialize_by(profile: @profile)
+    @opportunity_application.choices = session[:volunteer_opportunity_id] if session[:volunteer_opportunity_id] and @opportunity_application.new_record?
+    @opportunity_application.save(validate: false)
+    @volunteer_opportunities = policy_scope(VolunteerOpportunity).order(:title)
   end
 
   def success
@@ -13,5 +16,10 @@ class DashboardController < ApplicationController
     redirect_to apply_path, notice: 'Opportunity application was successfully created.' unless OpportunityApplication.find_by(profile: profile).submitted
 
     @update = params['update'] == 'true' || false
+  end
+
+  private
+  def store_opportunity_id_if_not_authorized_yet
+    session[:volunteer_opportunity_id] = [params['volunteer_opportunity_id']].to_json
   end
 end
