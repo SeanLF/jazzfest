07732bfb52ed534a703106af4b17e789ba9afd6a
Sean Floyd <seanfloyd@icloud.com>
2020-05-30 21:55:11 -0400

    Update generator templates

--- lib/helpers/generator_helper.rb
+++ lib/helpers/generator_helper.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Module that plugs in with form helpers to convert form methods to class names
 module GeneratorHelper
   def self.get_class_for_form_field(field_method)
     case field_method
